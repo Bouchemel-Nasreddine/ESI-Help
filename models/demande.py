3dc81c2f2c268d3e5c1a544dca4b009b969006c6
Bouchemel Nasreddine <bouchemelnasreddine@gmail.com>
2022-02-04 02:53:26 +0100

    demande buttons ui

--- models/demande.py
+++ models/demande.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import string
 from setuptools import Require
 from odoo import api, fields, models, _
 
@@ -9,6 +10,8 @@ class Demande(models.Model):
 
     _name = "esi_help.demande"
     _description = "desc"
+
+    etat_validation = False
 
     name = fields.Char(string='titre',  required=True)
     contenu = fields.Text(string='contenu', required=True)
@@ -20,3 +23,11 @@ class Demande(models.Model):
             ('médic', 'demande de médicament'),
         ], 
         required=True)
+    #etat_validation = fields.Boolean(_etat_validation)
+    def action_valider (self):
+        #self.etat_validation=True
+        print('mmmm')
+    def action_creer_projet (self):
+        if self.etat_validation :
+            print('mmm')
+        
